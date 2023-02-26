6ba29708c59959300d901d0f14d76997370d8662
cheeter_Lee <johnson991210@gmail.com>
2023-02-26 23:09:13 +0800

    UPDATE: enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors()
   await app.listen(3000);
   console.log(`connected to db listening on Port 3000`)
 }
