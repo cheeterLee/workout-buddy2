e086fc1f55d6c040d05c43806583de2565c50dd9
cheeter_Lee <johnson991210@gmail.com>
2023-02-28 13:34:40 +0800

    Modify switch theme button

--- frontend-web/src/components/Navbar.tsx
+++ frontend-web/src/components/Navbar.tsx
@@ -1,13 +1,18 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { BsPerson, BsTelephone } from 'react-icons/bs'
 import { BiHome, BiMoon, BiSun } from 'react-icons/bi'
 import Navlink from "./Navlink"
 import Navbutton from "./Navbutton"
-import ThemeSwitcher from "./ThemeSwitcher"
+import { useDarkMode } from "../hooks/useDarkMode"
 
 export interface INavbarProps {}
 
 const Navbar: React.FunctionComponent<INavbarProps> = (props) => {
+    const [darkTheme, setDarkTheme] = useDarkMode()
+
+    // @ts-ignore
+	const switchTheme = () => setDarkTheme(!darkTheme)
+
 	return (
 		<div className="bg-primary-400 dark:bg-primary-900 w-screen flex justify-center items-center h-24 gap-14
         drop-shadow-md">
@@ -22,7 +27,11 @@ const Navbar: React.FunctionComponent<INavbarProps> = (props) => {
 				</ul>
 			</div>
             <div className="flex items-center gap-1">
-                <ThemeSwitcher icon={<BiMoon />} />
+                {/* <ThemeSwitcher icon={darkTheme ? <BiMoon /> : <BiSun />} /> */}
+                <div onClick={switchTheme}
+			        className="bg-primary-300 dark:bg-primary-800 py-1 px-1 rounded-lg text-yellow-600 h-10 w-10 flex justify-center items-center cursor-pointer">
+                    {darkTheme ? <BiMoon /> : <BiSun />}
+                </div>
                 <Navbutton text='logout'/>
             </div>
 		</div>
