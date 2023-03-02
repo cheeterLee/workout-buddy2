01e853cf3cb5ae0f839bd1f8da12a8c26a9236a3
cheeter_Lee <johnson991210@gmail.com>
2023-03-02 15:07:35 +0800

    validate login logout navigation

--- frontend-web/src/components/Navbutton.tsx
+++ frontend-web/src/components/Navbutton.tsx
@@ -1,11 +1,33 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { setLogout } from '../state'
+import { RootState } from '../state/store'
 
 export interface INavbuttonProps {
     text: string
 }
 
 const Navbutton: React.FunctionComponent<INavbuttonProps> = ({ text }) => {
-    return <button className='bg-primary-300 hover:bg-primary-200 dark:bg-primary-800 dark:hover:bg-primary-700 py-1 px-3 rounded-lg text-primary-800 dark:text-yellow-500 h-10 w-28'>
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const user = useSelector((state: RootState) => state.auth.user)
+
+    const handleNavigateClicked = () => {
+        navigate('/login')
+    }
+
+    const handleLogoutClicked = () => {
+        dispatch(setLogout({
+            user: null,
+            token: null
+        }))
+        navigate('/login')
+    }
+
+    return <button 
+        onClick={user ? handleLogoutClicked : handleNavigateClicked}
+    className='bg-primary-300 hover:bg-primary-200 dark:bg-primary-800 dark:hover:bg-primary-700 py-1 px-3 rounded-lg text-primary-800 dark:text-yellow-500 h-10 w-28'>
         {text}
     </button>
 }
