616f1ed423005f165109786063b0f12d275ca997
cheeter_Lee <johnson991210@gmail.com>
2023-03-02 15:09:57 +0800

    embed backend PORT into env files

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors()
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
   console.log(`connected to db listening on Port 3000`)
 }
 bootstrap();
