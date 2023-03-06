83f76d40ed090cb71959d5009f783cbda7f14a73
cheeter_Lee <johnson991210@gmail.com>
2023-03-06 16:21:34 +0800

    modified calender layout

--- frontend-web/src/components/Calender.tsx
+++ frontend-web/src/components/Calender.tsx
@@ -1,13 +1,17 @@
 import React from "react"
+import { useAppSelector } from "../state/hooks"
 
 export interface ICalenderProps {}
 
 const Calender: React.FunctionComponent<ICalenderProps> = (props) => {
+	const user = useAppSelector(state => state.auth.user)
+
 	return (
 		<div className="bg-primary-300 dark:bg-primary-700 h-[300px] drop-shadow-md rounded-md p-2 flex flex-col justify-center">
 			<h3 className="text-primary-800 dark:text-yellow-400 text-center font-mono font-semibold py-1">
-				View Weely Activities
+				<span className=" text-teal-700 dark:text-orange-400">{user}'s</span> weekly activities
 			</h3>
+			<div className="text-center text-primary-600 dark:text-primary-400 text-sm">2023.3.6 - 2023.3.13</div>
 			<div className="grid grid-cols-7 h-[80%] p-2 bg-primary-400 dark:bg-primary-600 rounded-md">
 				<div className="outline-dashed outline-1 outline-primary-800 dark:outline-primary-400 grid">
 					<p className="m-auto text-primary-800">Mon</p>
