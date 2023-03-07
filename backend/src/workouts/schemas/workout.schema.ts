4aea81b427cd558455ea5939c27afd67333bd43f
cheeter_Lee <johnson991210@gmail.com>
2023-03-07 18:28:25 +0800

    modify backend workout schema

--- backend/src/workouts/schemas/workout.schema.ts
+++ backend/src/workouts/schemas/workout.schema.ts
@@ -3,6 +3,17 @@ import { HydratedDocument } from 'mongoose'
 
 export type WorkoutDocument = HydratedDocument<Workout>
 
+const formateDate = (date: Date): string => {
+	const padTo2Digits = (num: number): string => {
+		return num.toString().padStart(2, "0")
+	}
+	return [
+		date.getFullYear(),
+		padTo2Digits(date.getMonth() + 1),
+		padTo2Digits(date.getDate()),
+	].join(".")
+}
+
 @Schema({ timestamps: true })
 export class Workout {
     @Prop()
@@ -16,6 +27,9 @@ export class Workout {
 
     @Prop()
     username: string
+
+    @Prop({ default: formateDate(new Date(Date.now())) })
+    createdDate: string
 }
 
 export const WorkoutSchema = SchemaFactory.createForClass(Workout)
