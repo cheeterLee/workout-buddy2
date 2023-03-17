fdbeca24798a6c1dd29e79b6c02ef5fec04f1a8d
cheeter_Lee <johnson991210@gmail.com>
2023-03-17 14:25:34 +0800

    centered content in svg

--- frontend-web/src/components/Chart.tsx
+++ frontend-web/src/components/Chart.tsx
@@ -4,118 +4,77 @@ import * as d3 from "d3"
 export interface IChartProps {}
 
 const Chart: React.FunctionComponent<IChartProps> = (props) => {
-	const workouts = [1, 4, 3, 10, 3, 6, 1]
+	const workouts = [1, 3, 5, 6, 2, 4, 2]
+
 	const parentRef = useRef<any>()
 	const svgRef = useRef<any>()
 
 	const createGraph = async () => {
-		const w: number = parentRef.current.clientWidth
-		const h: number = parentRef.current.clientHeight
+		const width = 200
+		const height = 100
+
+		const margin = { top: 30, right: 30, bottom: 30, left: 30 }
 		const svg = d3
 			.select(svgRef.current)
-			.attr("width", 0.95 * w)
-			.attr("height", 0.95 * h)
+			.attr("width", width + margin.left + margin.right)
+			.attr("height", height + margin.top + margin.bottom)
+			.append('g')
+			.attr("transform", `translate(${margin.left}, ${margin.top})`)
 
 		const xScale = d3
 			.scaleLinear()
 			.domain([0, workouts.length - 1])
-			.range([0, 0.9 * w])
+			.range([0, width])
 
 		const yScale = d3
 			.scaleLinear()
-			.domain([0, d3.max(workouts) || 10]).nice()
-			.range([0.9 * h, 0])
-		
+			.domain([0, d3.max(workouts) || 10])
+			.range([height, 0])
+
+		const generateScaledLine = d3
+			.line<any>()
+			.x((d, i) => xScale(i))
+			.y((d) => yScale(d))
+			.curve(d3.curveCardinal)
+
 		const xAxis = d3
 			.axisBottom(xScale)
 			.ticks(workouts.length)
 			.tickFormat((i) => (i.valueOf() + 1).toString())
 
-		const yAxis = d3
-			.axisLeft(yScale)
-			.ticks(5)
-			.tickSize(-0.9 * w)
-			.tickPadding(10)
-			
-		svg.append("g").call(xAxis).attr("transform", `translate(0, ${0.88 * h})`)
+		const yAxis = d3.axisLeft(yScale).ticks(5)
 
-		svg.append("g").call(yAxis).attr("transform", `translate(${0.05 * w}, 0)`);
+		svg.append("g").call(xAxis).attr("transform", `translate(0, ${height})`)
 
-		const generateScaledLine = d3
-			.line<any>()
-			.x((d, i) => xScale(i))
-			.y(yScale)
-			.curve(d3.curveCardinal)
+		svg.append("g").call(yAxis)
 
-			svg.selectAll(".line")
-					.data([workouts])
-					.join("path")
-					.attr("d", (d) => generateScaledLine(d))
-					.attr("fill", "none")
-					.attr("stroke", "salmon")
-					.style("stroke-width", "3px")
-
+		svg.selectAll(".line")
+			.data([workouts])
+			.join("path")
+			.attr("d", (d) => generateScaledLine(d))
+			.attr("fill", "none")
+			.attr("stroke", "salmon")
+			.style("stroke-width", "5px")
 	}
 
 	useEffect(() => {
 		createGraph()
 	}, [])
 
-	// useEffect(() => {
-	// 	const w: number = parentRef.current.clientWidth
-	// 	const h: number = parentRef.current.clientHeight
-
-	// 	console.log("scale (w, h)", w, h)
-
-	// 	const svg = d3.select(svgRef.current).attr("width", w).attr("height", h)
-
-	// 	const xScale = d3
-	// 		.scaleLinear()
-	// 		.domain([0, workouts.length - 1])
-	// 		.range([0, w])
-	// 	const yScale = d3
-	// 		.scaleLinear()
-	// 		// .domain([0, h])
-	// 		.domain([0, d3.max(workouts) || 100])
-	// 		.range([h, 0])
-	// 	const generateScaledLine = d3
-	// 		.line<any>()
-	// 		.x((d, i) => xScale(i))
-	// 		.y(yScale)
-	// 		.curve(d3.curveCardinal)
-
-	// 	const xAxis = d3
-	// 		.axisBottom(xScale)
-	// 		.ticks(workouts.length)
-	// 		.tickFormat((i) => (i.valueOf() + 1).toString())
-
-	// 	const yAxis = d3.axisLeft(yScale).ticks(5)
-
-	// 	svg.append("g").call(xAxis).attr("transform", `translate(0, ${h})`)
-
-	// 	svg.append("g").call(yAxis)
-
-	// 	svg.selectAll(".line")
-	// 		.data([workouts])
-	// 		.join("path")
-	// 		.attr("d", (d) => generateScaledLine(d))
-	// 		.attr("fill", "none")
-	// 		.attr("stroke", "salmon")
-	// 		.style("stroke-width", "5px")
-	// 	}, [])
-
 	return (
 		<div className="w-full flex-1 flex justify-center items-center">
 			<div className="w-full h-full border-2 border-red-500 flex items-center justify-center">
 				<div
 					ref={parentRef}
-					className="w-[85%] h-[80%] border-2 border-teal-500 bg-primary-300 dark:bg-primary-600 rounded-md drop-shadow-lg flex items-center justify-center
-					px-10"
+					className="w-[85%] h-[80%] border-2 border-teal-500 bg-primary-300 dark:bg-primary-600 rounded-md drop-shadow-lg flex flex-col items-center justify-center"
 				>
-					<svg
-						// className="border-2 border-pink-500"
-						ref={svgRef}
-					></svg>
+					<div>Date right here</div>
+					<div className="w-full h-full flex-1 flex items-center justify-center">
+						<svg
+							className="border-2 border-pink-500"
+							ref={svgRef}
+						></svg>
+					</div>
 				</div>
 			</div>
 		</div>
